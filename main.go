75d66516c10b35a53c22e56f8fe757cf54b7aa6a
Rep <rizqyepr@gmail.com>
2025-05-23 03:09:34 +0700

    feat: add xml content

--- main.go
+++ main.go
@@ -76,6 +76,26 @@ func runMigrations() error {
 	return nil
 }
 
+var xmlContent = `
+		<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<url>
+  <loc>https://rizqyep.me/</loc>
+  <lastmod>2025-05-22T20:07:09+00:00</lastmod>
+  <priority>1.00</priority>
+</url>
+<url>
+  <loc>https://rizqyep.me/static/Rizqy%20Eka%20Putra%20Rizaldy_CV.pdf</loc>
+  <lastmod>2025-05-20T19:18:17+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+</urlset>
+`
+
 func main() {
 	// Serve static files
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
@@ -89,6 +109,11 @@ func main() {
 		w.Header().Set("Content-Type", "text/plain")
 		w.Write([]byte("User-agent: *\nAllow: /"))
 		w.Write([]byte("Disallow: /static/"))
+	})
+
+	http.HandleFunc("/sitemap.xml", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/xml")
+		w.Write([]byte(xmlContent))
 	})
 
 	log.Println("Server starting on :8110...")
